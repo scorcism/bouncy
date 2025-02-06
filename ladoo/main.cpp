47187b902627c2522291d084f71055b6b31211f1
scorcism <abhishekpathak1720@gmail.com>
2025-02-07 00:11:14 +0530

    Add balls, default velocity to ball

--- ladoo/main.cpp
+++ ladoo/main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+
+const float GRAVITY = 9.8f;
+const float DAMPING = 0.8f;
+const float BALL_RADIUS = 55.0f;
+
+
+struct Ball {
+	sf::CircleShape shape;
+	sf::Vector2f velocity;
+	bool isDragging = false;
+
+	Ball(float x, float y) {
+		shape.setRadius(BALL_RADIUS);
+		shape.setFillColor(sf::Color::Red);
+		shape.setPosition({ x, y });
+	}
+};
+
 int main(){
 	
-	unsigned int width = 640;
-	unsigned int height = 360;
+	unsigned int width = 800;
+	unsigned int height = 600;
 
-	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width, height }), "Ladoo");
+	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width, height }), "Bouncy");
 	window->setFramerateLimit(60);
-
+	
+	std::vector<Ball> balls;
+	Ball* selectedBall = nullptr;
 
 	while (window->isOpen()) {
 		while (const std::optional event = window->pollEvent()){
@@ -20,13 +40,47 @@ int main(){
 					window->close();
 				}
 			}
+			if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>()) {
+				sf::Vector2i mousePosition = mouseButtonPressed->position;
+				sf::Vector2f mousePositionFloat = static_cast<sf::Vector2f>(mousePosition);
+				if (mouseButtonPressed->button == sf::Mouse::Button::Left) {
+					
+						balls.emplace_back(mousePosition.x - BALL_RADIUS, mousePosition.y - BALL_RADIUS);
+				}else if (mouseButtonPressed->button == sf::Mouse::Button::Right) {
+					for (auto it = balls.begin(); it != balls.end(); ++it) {
+						if (it->shape.getGlobalBounds().contains(mousePositionFloat)) {
+							balls.erase(it);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		//Add gravity to the balls
+		for (auto& ball : balls) {
+			if (!ball.isDragging) {
+				ball.velocity.y += GRAVITY;
+				sf::Vector2f pos = ball.shape.getPosition();
+				pos += ball.velocity;
+
+				if (pos.y + 2 * BALL_RADIUS > window->getSize().y) {
+					pos.y = window->getSize().y - 2 * BALL_RADIUS;
+					ball.velocity.y = -ball.velocity.y * DAMPING;
+				}
+
+				ball.shape.setPosition(pos);
+			}
 		}
 
 
 		//Render
-		window->clear();
+		window->clear(sf::Color::Black);
 
 		//Drawing here
+		for (const auto& ball : balls) {
+			window->draw(ball.shape);
+		}
 
 		window->display();
 	}
